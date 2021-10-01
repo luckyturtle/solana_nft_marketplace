e3e795878af4c8f87624d7a7559e75ba12f09076
PandaDevA <mianhua9999@gmail.com>
2021-10-01 03:45:42 +0300

    real server

--- js/packages/web/src/config/api.js
+++ js/packages/web/src/config/api.js
@@ -1,3 +1,3 @@
- export const baseURL = "https://103.195.4.235:4040";
-// export const baseURL = "https://192.168.114.92:4040";
+//  export const baseURL = "https://103.195.4.235:4040";
+export const baseURL = "https://199.19.72.225:4040";
 // export const baseURL = "https://api.cappacollectibles.com";
